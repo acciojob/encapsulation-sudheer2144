a37bcc4f8a732a8276824ae7a2b34361b1d88f61
Sudheer <sudheer.ged@gmail.com>
2022-12-10 12:26:48 +0530

    Initial commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,17 @@
 package com.driver;
-
+import java.util.*;
 public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        RWOnly rwo=new RWOnly();
+        String name=sc.nextLine();
+        //rwo.name=name;
+        //System.out.println(rwo.name);
+        //'name' has private access in 'com.driver.RWOnly'
+
+        rwo.setter(name);
+        System.out.println(rwo.getter());
+    }
   
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,17 @@
 package com.driver;
 
 public class RWOnly {
+
+    private String name;
+
+    public void setter(String name)
+    {
+        this.name = name;
+    }
+
+    public String getter()
+    {
+        return name;
+    }
     
 }
